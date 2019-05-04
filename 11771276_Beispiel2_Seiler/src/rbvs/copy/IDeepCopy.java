01b7be4c377ff9b4edfcde2feb692bc17f9b06f4
DarkTank2 <x.s@gmx.at>
2019-05-04 16:45:59 +0200

    Initial commit with first interface

---
+++ 11771276_Beispiel2_Seiler/src/rbvs/copy/IDeepCopy.java
@@ -0,0 +1,11 @@
+/**
+ * Filename: IDeepCopy.java
+ * Description: 
+ * @author Alexander Seiler, 11771276
+ * @since 04.05.2019
+ */
+package rbvs.copy;
+
+public interface IDeepCopy {
+	IDeepCopy deepCopy();
+}
